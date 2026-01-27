71303b3fbf74e05fae251968cdd56026c3a0cab8
chronos3344 <chronos334@gmail.com>
2026-01-27 13:24:59 +0300

    feature/TASK-1001 fixing

--- main.go
+++ main.go
@@ -1,27 +1,3 @@
-//package main
-//
-//import (
-//	"log"
-//
-//	"github.com/chronos3344/catalog-service/internal/app/config"
-//	_ "github.com/chronos3344/catalog-service/internal/app/config/section"
-//	rhealth "github.com/chronos3344/catalog-service/internal/app/handler/health"
-//	rprocessor "github.com/chronos3344/catalog-service/internal/app/processor/http"
-//)
-//
-//func main() {
-//	cfg, err := config.Load()
-//	if err != nil {
-//		log.Fatal("Failed to load config:", err)
-//	}
-//
-//	healthHandler := rhealth.NewHandler()
-//
-//	httpServer := rprocessor.NewHttp(healthHandler, cfg.ProcessorWebServer)
-//
-//	log.Fatal("HTTP server error:", httpServer.Serve())
-//}
-
 package main
 
 import (
