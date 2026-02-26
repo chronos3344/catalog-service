88b9783b1d88b408df762f4316f9ae92e8912669
chronos3344 <chronos334@gmail.com>
2026-02-26 14:03:23 +0300

    TASK-1005: Исправление ошибок

--- internal/app/repository/repository.go
+++ internal/app/repository/repository.go
@@ -17,10 +17,10 @@ type Category interface {
 }
 
 type Product interface {
-	Create(ctx context.Context, product entity.Product) (entity.Product, error)
+	Create(ctx context.Context, name string, price float64, categoryGUID uuid.UUID, description *string) (entity.Product, error)
 	GetByGUID(ctx context.Context, guid uuid.UUID) (entity.Product, error)
 	GetByName(ctx context.Context, name string) (entity.Product, error)
-	List(ctx context.Context, filter entity.RequestProductList) ([]entity.Product, error)
-	Update(ctx context.Context, product entity.ResponseProductUpdate) (entity.ResponseProductUpdate, error)
+	List(ctx context.Context, categoryGUID *uuid.UUID, minPrice *float64, maxPrice *float64) ([]entity.Product, error)
+	Update(ctx context.Context, guid uuid.UUID, name *string, price *float64, categoryGUID *uuid.UUID, description *string) (entity.ResponseProductUpdate, error)
 	Delete(ctx context.Context, guid uuid.UUID) error
 }
