d243fae90a92d735c56295898d8f2c95f4e2e4c6
chronos3344 <chronos3344@github.com>
2026-05-25 20:17:47 +0300

    TASK-2004: HTTP-сервер и Graceful Shutdown созданы

--- internal/app/processor/http/http.go
+++ internal/app/processor/http/http.go
@@ -2,12 +2,15 @@ package rprocessor
 
 import (
 	"context"
+	"net"
 	"net/http"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/chronos3344/catalog-service/internal/app/config/section"
 	rhandler "github.com/chronos3344/catalog-service/internal/app/handler"
+	"github.com/chronos3344/catalog-service/internal/app/processor"
 	"github.com/chronos3344/catalog-service/internal/app/util"
 	"github.com/chronos3344/catalog-service/internal/pkg/http/httph"
 	"github.com/chronos3344/catalog-service/internal/pkg/http/mzerolog"
@@ -19,7 +22,7 @@ type httpProc struct {
 	server *http.Server
 }
 
-func NewHttp(hHealth rhandler.Health, hCategory rhandler.Category, hProduct rhandler.Product, cfg section.ProcessorWebServer) *httpProc {
+func NewHttp(hHealth rhandler.Health, hCategory rhandler.Category, hProduct rhandler.Product, cfg section.ProcessorWebServer) processor.Processor {
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(handlerNotFound)
 
@@ -65,12 +68,44 @@ func NewHttp(hHealth rhandler.Health, hCategory rhandler.Category, hProduct rhan
 	}
 }
 
-func (h *httpProc) Serve() error {
-	log.Info().Msg("Starting HTTP server")
-	return h.server.ListenAndServe()
+func (p *httpProc) StartAsync(ctx context.Context, wg *sync.WaitGroup) {
+	lc := net.ListenConfig{}
+	listener, err := lc.Listen(ctx, "tcp", p.server.Addr)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to create listener")
+	}
+
+	log.Info().Str("addr", p.server.Addr).Msg("HTTP server started successfully")
+
+	go func() {
+		if err := p.server.Serve(listener); err != nil && err != http.ErrServerClosed {
+			log.Error().Err(err).Msg("HTTP server serve error")
+		}
+	}()
+
+	processor.WatchForShutdown(ctx, wg, processor.CloserFunc(func() error {
+		log.Info().Msg("Closing HTTP listener...")
+		return listener.Close()
+	}))
+
+	shutdownTimeout := 5 * time.Second
+	processor.WatchForShutdown(ctx, wg, processor.NewCloserContextFunc(
+		func(shutdownCtx context.Context) error {
+			log.Info().Msg("Shutting down HTTP server gracefully...")
+			return p.server.Shutdown(shutdownCtx)
+		},
+		ctx,
+		shutdownTimeout,
+	))
 }
 
-func (h *httpProc) Shutdown(ctx context.Context) error {
+//func (p *httpProc) serve(l net.Listener) error {
+//	_ = p.server.Serve(l)
+//	log.Info().Msg("Starting HTTP server")
+//	return p.server.ListenAndServe()
+//}
+
+func (p *httpProc) Shutdown(ctx context.Context) error {
 	log.Info().Msg("Shutting down HTTP server...")
-	return h.server.Shutdown(ctx)
+	return p.server.Shutdown(ctx)
 }
